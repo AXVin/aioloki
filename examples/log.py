615ca8b52900bfdc53aa018dcf4eede022a3a416
AXVin <35896758+AXVin@users.noreply.github.com>
2021-08-03 14:55:55 +0200

    Adds an example

---
+++ examples/log.py
@@ -0,0 +1,22 @@
+import asyncio
+import logging
+import aiohttp
+import aioloki
+
+async def main():
+    session = aiohttp.ClientSession()
+    handler = aioloki.AioLokiHandler(
+        'http://localhost:3100',
+        tags={'cluser': '1'},
+        session=session
+    )
+    log = logging.getLogger('test-logging')
+    log.addHandler(handler)
+    log.info(
+        'Setup aioloki successfully',
+        extra={'tags': {'function': 'main'}}
+    )
+    await session.close()
+
+asyncio.run(main())
+
